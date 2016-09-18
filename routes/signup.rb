0af10e4bfd36f70f3cc5c6bb4c586938755eee1a
Hugo FABRE <hugo.fabre@epitech.eu>
2016-09-18 16:09:38 +0200

    [feat]Group all sign up in one route

--- routes/signup.rb
+++ routes/signup.rb
@@ -1,36 +1,45 @@
 class Spred < Sinatra::Application
   # Multiple step sign up
-  get '/signup_step1' do
+  get '/signup-step1' do
     haml :signup_step1, :layout => :sign_layout
   end
 
-  get '/signup_step2' do
+  get '/signup-step2' do
     @title = 'Get pseudo'
     haml :signup_step2
   end
 
-  get '/signup_step3' do
+  get '/signup-step3' do
     @title = 'Register subject you like'
     haml :signup_step3
   end
 
-  post '/signup_step1' do
-    session[:futur_user] = {url: ApiEndPoint::SIGNUP}
-    if params[:password] != params[:confirm_password]
-      flash[:error] = 'Password does not match'
-      redirect '/signup'
-    end
-    verified_params = params.select {|k,_| [:email, :password, :first_name, :last_name].include?(k.to_sym) }
-    session[:futur_user] = verified_params
-    redirect '/signup_step2'
+  post '/signup-step1' do
+    session[:futur_user] = case params[:signup_type]
+                        when 'google_token'
+                          {url: ApiEndPoint::GOOGLE_SIGNUP, access_token: params[:token]}
+                        when 'facebook_token'
+                          {url: ApiEndPoint::FACEBOOK_SIGNUP, access_token: params[:token]}
+                        when 'password'
+                          session[:futur_user] = {url: ApiEndPoint::SIGNUP}
+                          if params[:password] != params[:confirm_password]
+                            flash[:error] = 'Password does not match'
+                            redirect '/signup-step1'
+                          end
+                          params.select {|k,_| [:email, :password, :first_name, :last_name].include?(k.to_sym) }
+                        else
+                          flash[:error] = 'Invalid signup type'
+                          redirect '/signup-step1'
+                      end
+    redirect '/signup-step2'
   end
 
-  post '/signup_step2' do
+  post '/signup-step2' do
     session[:futur_user].merge!(params[:pseudo])
-    redirect '/signup_step3'
+    redirect '/signup-step3'
   end
 
-  post '/signup_step3' do
+  post '/signup-step3' do
     user = session[:futur_user]#.merge(params[:interesting_subject_ids])
     req = PostRequest.new(session, :login, session[:futur_user][:url], user.tap {|u| u.delete(:url)})
     response = req.send
@@ -40,18 +49,5 @@ class Spred < Sinatra::Application
     end
     flash[:success] = 'Successfully signed up'
     redirect '/'
-  end
-
-  # Multiple step social network sign up
-  post '/google_signup' do
-    session[:futur_user] = {url: ApiEndPoint::GOOGLE_SIGNUP}
-    session[:futur_user][:access_token] = params[:token]
-    redirect '/signup_step2'
-  end
-
-  post '/facebook_signup' do
-    session[:futur_user] = {url: ApiEndPoint::FACEBOOK_SIGNUP}
-    session[:futur_user][:access_token] = params[:token]
-    redirect '/signup_step2'
   end
 end
