0e25b8da18e87d340a618909eac1b3f2938b2f23
Hugo FABRE <hugo.fabre@epitech.eu>
2016-09-29 23:14:17 +0200

    [feat] add inbox routes

---
+++ routes/inbox.rb
@@ -0,0 +1,58 @@
+class Spred
+  include Authentication
+
+  get '/inbox' do
+    request = GetInboxRequest.new(session[:spred_tokens])
+    request.send
+    @response = request.parse_response
+    if @response.is_a? APIError
+      @errors[:default] = @response.message
+    else
+      @response
+    end
+  end
+
+  get '/inbox/conversation/:id' do
+    request = GetConversationRequest.new(session[:spred_tokens], params[:id])
+    request.send
+    @response = request.parse_response
+    if @response.is_a? APIError
+      @errors[:default] = @response.message
+    else
+      @response
+    end
+  end
+
+  get '/inbox/conversation/:conversation_id/message/:message_id' do
+    request = GetMessageRequest.new(session[:spred_tokens], params[:conversation_id], params[:message_id])
+    request.send
+    @response = request.parse_response
+    if @response.is_a? APIError
+      @errors[:default] = @response.message
+    else
+      @response
+    end
+  end
+
+  post '/inbox/conversation' do
+    request = CreateConversationRequest.new(session[:spred_tokens], params[:conversation])
+    request.send
+    @response = request.parse_response
+    if @response.is_a? APIError
+      @errors[:default] = @response.message
+    else
+      @response
+    end
+  end
+
+  post '/inbox/conversation/:id' do
+    request = CreateMessageRequest.new(session[:spred_tokens], params[:id], params[:message])
+    request.send
+    @response = request.parse_response
+    if @response.is_a? APIError
+      @errors[:default] = @response.message
+    else
+      @response
+    end
+  end
+end
