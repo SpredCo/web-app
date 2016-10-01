46c9028f254c4eea0af008d8e535dbfc14a29b33
Hugo FABRE <hugo.fabre@epitech.eu>
2016-10-01 15:53:32 +0200

    merging refactoring into master

---
+++ public/js/login.js
@@ -0,0 +1,49 @@
+// Load the SDK asynchronously
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '565509136962999',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.7'
+    });
+};
+
+function fbLogin() {
+    FB.login(function (responses) {
+        if (responses.authResponse) {
+            $('#login-type').val('facebook_token');
+            $('#token').val(responses.authResponse.accessToken);
+            $('#login-form').submit();
+        }
+    }, {
+        'scope': 'email'
+    });
+}
+
+function attachSignin(element) {
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+            $('#login-type').val('google_token');
+            $('#token').val(googleUser.Zi.access_token);
+            $('#login-form').submit();
+        }, function(error) {
+        });
+}
+
+$(document).ready(function () {
+    gapi.load('auth2', function() {
+        // Retrieve the singleton for the GoogleAuth library and set up the client.
+        auth2 = gapi.auth2.init({
+            client_id: '220029720276-eg21k8f0ui6ephhh6n0ec6o3il6oip1l.apps.googleusercontent.com'
+        });
+        attachSignin(document.getElementById('googleLogin'));
+    });
+});
