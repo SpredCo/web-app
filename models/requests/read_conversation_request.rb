6c6a784ff080e46181cb1ee0017b8fac25d209de
Hugo FABRE <hugo.fabre@epitech.eu>
2016-10-13 19:20:41 +0200

    [fix]crash on read conversation

--- models/requests/read_conversation_request.rb
+++ models/requests/read_conversation_request.rb
@@ -1,6 +1,6 @@
 class ReadConversationRequest < PostRequest
   def initialize(tokens, id, read=true)
-    req = API::Request::READ_CONVERSATION
+    req = APIHelper::Request::READ_CONVERSATION
     super(tokens, req[:service], "#{req[:end_point]}/#{id}/read", {read: read})
   end
 end
