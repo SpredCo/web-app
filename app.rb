fd4d7a4b20b98bfa2d48ba59393a3c2d70e8a530
Hugo FABRE <hugo.fabre@epitech.eu>
2016-10-16 18:44:26 +0200

    hotfix

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'json'
 require 'haml'
-require 'better_errors' if ENV['RACK_ENV'] != :production
+#require 'better_errors' if ENV['RACK_ENV'] != :production
 
 class Spred < Sinatra::Application
   use Rack::Session::Pool
