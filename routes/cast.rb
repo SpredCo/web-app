def05f489dd642589c59a3081827b1c3996d228b
Hugo Fabre <hugo.fabre@epitech.eu>
2016-11-20 19:21:08 +0100

    add route to get cast token

--- routes/cast.rb
+++ routes/cast.rb
@@ -1,40 +1,47 @@
-get '/create-cast' do
-  authenticate!
-  @unread_message_count = synchronize_inbox!
-  @title = 'Spred'
-  haml :'cast/create', layout: :'layout/create_cast_layout'
-end
-
-post '/create-cast' do
-  authenticate!
-  params.delete('validate')
-  params.delete('search')
-  is_public = params.delete('cast-type') == 'public'
-  date_value = params.delete('date-value')
-  cast_date = params.delete('date') == 'now' ? 'now' : DateTime.parse(date_value).to_s
-
-  picture = if params['picture']
-    pic_path = CastHelper.generate_uniq_cover_url(params['picture'][:type].split('/')[1])
-    CastHelper.save_cover(pic_path, params['picture'][:tempfile])
-    "#{request.base_url}/#{pic_path}"
+class Spred
+  get '/create-cast' do
+    authenticate!
+    @unread_message_count = synchronize_inbox!
+    @title = 'Spred'
+    haml :'cast/create', layout: :'layout/create_cast_layout'
   end
 
-  params['tags'] = params['tags'].split(',')
-  req = CreateCastRequest.new(session[:spred_tokens], params.delete('name'), params.delete('description'), is_public, cast_date, params.merge({cover_url: picture}))
-  req.send
-  req.parse_response
-  redirect '/'
-end
+  post '/create-cast' do
+    authenticate!
+    params.delete('validate')
+    params.delete('search')
+    is_public = params.delete('cast-type') == 'public'
+    date_value = params.delete('date-value')
+    cast_date = params.delete('date') == 'now' ? 'now' : DateTime.parse(date_value).to_s
 
-get '/casts/:url' do
-  tokens = session[:spred_tokens]
-  cast_req = GetCastRequest.new(tokens, params[:url])
-  cast_req.send
-  @cast = cast_req.parse_response.body
-  if @cast && !@cast.is_a?(APIError)
-    @cast = SpredCast.from_hash(tokens, @cast)
-    haml :'cast/show', layout: :'layout/cast_layout'
-  else
+    picture = if params['picture']
+                pic_path = CastHelper.generate_uniq_cover_url(params['picture'][:type].split('/')[1])
+                CastHelper.save_cover(pic_path, params['picture'][:tempfile])
+                "#{request.base_url}/#{pic_path}"
+              end
+
+    params['tags'] = params['tags'].split(',')
+    req = CreateCastRequest.new(session[:spred_tokens], params.delete('name'), params.delete('description'), is_public, cast_date, params.merge({cover_url: picture}))
+    req.send
+    req.parse_response
     redirect '/'
+  end
+
+  get '/casts/:url' do
+    tokens = session[:spred_tokens]
+    cast_req = GetCastRequest.new(tokens, params[:url])
+    cast_req.send
+    @cast = cast_req.parse_response.body
+    if @cast && !@cast.is_a?(APIError)
+      @cast = SpredCast.from_hash(tokens, @cast)
+      haml :'cast/show', layout: :'layout/cast_layout'
+    else
+      redirect '/'
+    end
+  end
+
+  get '/casts/token/:id' do
+    token = CastHelper.get_cast_token(tokens, params[:id])
+    @cast_token = CastToken.from_hash(token.body)
   end
 end
