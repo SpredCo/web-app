264fe039287de1a87183ca958513a5bf695e617c
Maxime Guedj <maxime@guedjdev.com>
2016-11-24 17:31:34 +0100

    intermediate

--- routes/cast.rb
+++ routes/cast.rb
@@ -24,7 +24,7 @@ class Spred
     req = CreateCastRequest.new(session[:spred_tokens], params.delete('name'), params.delete('description'), is_public, cast_date, params.merge({cover_url: picture}))
     req.send
     req.parse_response
-    redirect '/'
+    redirect '/profile/casts'
   end
 
   get '/casts/:url' do
