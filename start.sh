4815face3446b2f2282976a794de041988553c66
Hugo Fabre <hugo.fabre@epitech.eu>
2016-11-26 13:41:33 +0100

    hotfix

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ -z "$1" ]
 then
-    rerun "RACK_ENV='development' thin -R config.ru start --ssl --ssl-key-file ~/.ssl/spred.key --ssl-cert-file ~/.ssl/spred.crt"
+    RACK_ENV='development' thin -R config.ru start --ssl --ssl-key-file ./.ssl/spred.key --ssl-cert-file ./.ssl/spred.crt
 else
     RACK_ENV="$1" thin -R config.ru start
 fi
