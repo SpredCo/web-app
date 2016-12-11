bb47f7ae896ef226d6de5e316736e85015b49749
Hugo Fabre <hugo.fabre@epitech.eu>
2016-12-11 23:16:40 +0100

    hotfix

--- models/requests/get_cast_token_request.rb
+++ models/requests/get_cast_token_request.rb
@@ -1,6 +1,6 @@
 class GetCastTokenRequest < PostRequest
   def initialize(tokens, cast_id, presenter=nil)
     req = APIHelper::Request::MY_CAST
-    super(tokens, req[:service], "#{req[:end_point]}/#{cast_id}/token", {presenter: presenter})
+    super(tokens, req[:service], "#{req[:end_point]}/#{cast_id}/token")
   end
 end
