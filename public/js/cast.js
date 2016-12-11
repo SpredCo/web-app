144252249a179888824d795ca9991b6a00b85ed1
Guillaume Galas <guillaume.galas@epitech.eu>
2016-12-11 22:08:15 +0100

    cast.js file fix

--- public/js/cast.js
+++ public/js/cast.js
@@ -3,7 +3,6 @@ var client = null;
 $(document).ready(function() {
 	client = new Spred.Client();
 	var castId = $("#castId").val();
-	alert(castId);
 	client.on('messages', receiveMessage);
 	client.on('questions', receiveQuestion);
 	client.connect(castId);
@@ -22,28 +21,29 @@ function sendMessage() {
 	if (message != '') {
 		client.sendMessage(message);
 	}
+}
 
-	function receiveQuestion(question) {
-		const questionHtml =
-			'<div id="question-' + question.id + '" class="question">' +
-			'<div class="row">' +
-			'<div class="col-sm-2">' +
-			'<img class="question-picture" src="' + "question.user_picture" + '"/>' +
-			'</div>' +
-			'<div class="col-sm-10">' +
-			'<div class="question-info">Posée par <a href="/@' + question.sender + '">@' + question.sender + '</a> à ' + question.date.getUTCHours() + ':' + question.date.getMinutes() + '</div>' +
-			'<div class="question-content">' + question.text + '</div>' +
-			'</div>' +
-			'</div>' +
-			'</div>' +
-			'</div>';
-		$('#question-box').append(questionHtml).scrollTop($('#question-' + question.id).offset().top);
-	}
+function receiveQuestion(question) {
+	const questionHtml =
+		'<div id="question-' + question.id + '" class="question">' +
+		'<div class="row">' +
+		'<div class="col-sm-2">' +
+		'<img class="question-picture" src="' + "question.user_picture" + '"/>' +
+		'</div>' +
+		'<div class="col-sm-10">' +
+		'<div class="question-info">Posée par <a href="/@' + question.sender + '">@' + question.sender + '</a> à ' + question.date.getUTCHours() + ':' + question.date.getMinutes() + '</div>' +
+		'<div class="question-content">' + question.text + '</div>' +
+		'</div>' +
+		'</div>' +
+		'</div>' +
+		'</div>';
+	$('#question-box').append(questionHtml).scrollTop($('#question-' + question.id).offset().top);
+}
 
-	function receiveMessage(message) {
-		const messageHtml =
-			'<p id="message-' + message.id + '" class="message">' +
-			'<a href="/@' + message.sender + '">@' + message.sender + '</a> : ' + message.text +
-			'</p>';
-		$('#chat-box').append(messageHtml).scrollTop($('#message-' + message.id).offset().top);
-	}
+function receiveMessage(message) {
+	const messageHtml =
+		'<p id="message-' + message.id + '" class="message">' +
+		'<a href="/@' + message.sender + '">@' + message.sender + '</a> : ' + message.text +
+		'</p>';
+	$('#chat-box').append(messageHtml).scrollTop($('#message-' + message.id).offset().top);
+}
