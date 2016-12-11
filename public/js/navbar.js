16d3aacedd804f4ff33cd9e889b5cc9d7f29b445
Hugo Fabre <hugo.fabre@epitech.eu>
2016-12-11 23:56:48 +0100

    fix(autocomplete)

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,21 +1,26 @@
-var client = algoliasearch('KGZYQKI2SD', 'a8583e100dbd3bb6e5a64d76462d1f5b');
-var index = client.initIndex('tags');
+$( document ).ready(function() {
+    var client = algoliasearch('KGZYQKI2SD', 'a8583e100dbd3bb6e5a64d76462d1f5b');
+    var index = client.initIndex('global');
 
-$('#global-search').autocomplete({hint: false}, [
-    {
-        source: $.fn.autocomplete.sources.hits(index, { hitsPerPage: 5 }),
-        //value to be displayed in input control after user's suggestion selection
-        displayKey: 'name',
-        //hash of templates used when rendering dataset
-        templates: {
-            //'suggestion' templating function used to render a single suggestion
-            suggestion: function(suggestion) {
-                return '<span>' +
-                    suggestion._highlightResult.name.value + '</span><span>' +
-                    suggestion._highlightResult.team.value + '</span>';
+    $('#aa-input-search').autocomplete({hint: false, debug: true}, [
+        {
+            source: function (q, cb) {
+                index.search(q, {hitsPerPage: 5}, function (error, content) {
+                    if (error) {
+                        cb([]);
+                        return;
+                    }
+                    cb(content.hits, content);
+                });
+            },
+            displayKey: 'name',
+            templates: {
+                suggestion: function (suggestion) {
+                    return suggestion._highlightResult.name.value;
+                }
             }
         }
-    }
-]).on('autocomplete:selected', function(event, suggestion, dataset) {
-    console.log(suggestion, dataset);
+    ]).on('autocomplete:selected', function (event, suggestion, dataset) {
+        console.log(suggestion, dataset);
+    })
 });
