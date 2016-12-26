fd657fad0d75a5bc0f1f0ff66f7e1e1b8545f5af
Hugo Fabre <hfabre@synbioz.com>
2016-12-26 15:50:09 +0100

    add env configuration

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ -z "$1" ]
 then
-    RACK_ENV='development' thin -R config.ru start --ssl --ssl-key-file ./.ssl/spred.key --ssl-cert-file ./.ssl/spred.crt
+    RACK_ENV='development' thin -R config.ru -p $WEB_PORT start --ssl --ssl-key-file $WEB_SSL_KEY --ssl-cert-file $WEB_SSL_CERT
 else
-    RACK_ENV="$1" thin -R config.ru start
+    RACK_ENV="$1" thin -R config.ru start --ssl --ssl-key-file ./.ssl/spred.key --ssl-cert-file ./.ssl/spred.crt
 fi
