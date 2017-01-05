6378f56ecfdb3b79582d18e9f913ed0936f92d0c
Guillaume Galas <guillaume.galas@epitech.eu>
2017-01-05 01:48:48 +0100

    sorted questions

--- public/js/cast.js
+++ public/js/cast.js
@@ -38,8 +38,8 @@ $(document).ready(function() {
 	if (!castToken) {
 		client.on('messages', receiveMessage);
 		client.on('questions', receiveQuestion);
-		client.on('up_question', receiveQuestionUp);
-		client.on('down_question', receiveQuestionDown);
+		client.on('up_question', updateQuestions);
+		client.on('down_question', updateQuestions);
 		client.on('user_joined', updateUser);
 		client.on('user_left', updateUser);
 	}
@@ -102,46 +102,38 @@ function voteQuestion(way, id) {
 }
 
 function receiveQuestion(question) {
-	client.sendNotification(question);
-	const questionHtml =
-		'<div id="question-' + question.id + '" class="question">' +
-		'<div class="row">' +
-		'<div class="col-sm-2">' +
-		'<img class="question-picture" src="' + question.user_picture + '"/>' +
-		'</div>' +
-		'<div class="col-sm-8">' +
-		'<div class="question-info">Posée par <a href="/@' + question.sender + '">@' + question.sender + '</a> à ' + question.date.getUTCHours() + ':' + question.date.getMinutes() + '</div>' +
-		'<div class="question-content">' + question.text + '</div>' +
-		'</div>' +
-		'<div class="col-sm-1">' +
-		'<div class="question-vote-box"><span id="question-' + question.id + '-nbvote" class="question-vote-nb">' + question.nbVote + '</span><br/><span>vote(s)</span></div>' +
-		'</div>' +
-		'<div class="col-sm-1" style="margin-top: 5px">' +
-		'<button id="question-' + question.id + '-up-btn" class="btn btn-success question-btn-vote" onclick="voteQuestion(\'up\', ' + question.id + ');"><span class="glyphicon glyphicon-arrow-up"></span></button>' +
-		'<button id="question-' + question.id + '-down-btn" class="btn btn-danger question-btn-vote" onclick="voteQuestion(\'down\', ' + question.id + ');"><span class="glyphicon glyphicon-arrow-down"></span></button>' +
-		'</div>' +
-		'</div>' +
-		'</div>' +
-		'</div>';
-	$('#question-box').append(questionHtml).scrollTop($('#question-' + question.id).offset().top);
+	if (question) {
+		client.sendNotification(question);
+		updateQuestions();
+	}
 }
 
-function receiveQuestionUp(question) {
-	client.spredCast.questions.forEach(function(q) {
-		if (q.id === question.id) {
-			$('#question-' + q.id + '-nbvote').text(q.nbVote);
-			return;
-		}
+function updateQuestions() {
+	var questionsHtml = "";
+	client.spredCast.questions.forEach(function(question) {
+		const questionHtml =
+			'<div id="question-' + question.id + '" class="question">' +
+			'<div class="row">' +
+			'<div class="col-sm-2">' +
+			'<img class="question-picture" src="' + question.user_picture + '"/>' +
+			'</div>' +
+			'<div class="col-sm-8">' +
+			'<div class="question-info">Posée par <a href="/@' + question.sender + '">@' + question.sender + '</a> à ' + question.date.getUTCHours() + ':' + question.date.getMinutes() + '</div>' +
+			'<div class="question-content">' + question.text + '</div>' +
+			'</div>' +
+			'<div class="col-sm-1">' +
+			'<div class="question-vote-box"><span id="question-' + question.id + '-nbvote" class="question-vote-nb">' + question.nbVote + '</span><br/><span>vote(s)</span></div>' +
+			'</div>' +
+			'<div class="col-sm-1" style="margin-top: 5px">' +
+			'<button id="question-' + question.id + '-up-btn" class="btn btn-success question-btn-vote" onclick="voteQuestion(\'up\', ' + question.id + ');"><span class="glyphicon glyphicon-arrow-up"></span></button>' +
+			'<button id="question-' + question.id + '-down-btn" class="btn btn-danger question-btn-vote" onclick="voteQuestion(\'down\', ' + question.id + ');"><span class="glyphicon glyphicon-arrow-down"></span></button>' +
+			'</div>' +
+			'</div>' +
+			'</div>' +
+			'</div>';
+		questionsHtml = questionsHtml.concat(questionHtml);
 	});
-}
-
-function receiveQuestionDown(question) {
-	client.spredCast.questions.forEach(function(q) {
-		if (q.id === question.id) {
-			$('#question-' + question.id + '-nbvote').text(question.nbVote);
-			return;
-		}
-	});
+	$('#question-box').html(questionsHtml);
 }
 
 function receiveMessage(message) {
